40c7199d9fd007cdffa292dbfa1f27bbc2f6de7d
Benjamin Knox <bknox.contact@gmail.com>
2017-11-04 15:46:58 -0400

    Finish hero

--- assets/js/components/hero.jsx
+++ assets/js/components/hero.jsx
@@ -20,8 +20,14 @@ export default class Hero extends React.Component {
     return (
       <div className="content">
         <h1>{this.state.page.header}</h1>
-        <div className="hero-svg"></div>
+        <img className="hero-svg" src={require('../../images/hero.svg')}/>
         <h2>{this.state.page.tagLine}</h2>
+        <div className="arrow">
+          <svg width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="none"> 
+            <path className="white-path-shadow" d="M0,2 V 30 L 50,100 50,100 100,30 V 2 H 0 Z" vectorEffect="non-scaling-stroke"/> 
+            <path className="white-path" d="M0,0 V 10 L 50,80 50,80 100,10 V 0 H 0 Z" vectorEffect="non-scaling-stroke"/> 
+          </svg>
+        </div>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
     host: '0.0.0.0',
     headers: {
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Methods": "GET",
       "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
     }
   },
