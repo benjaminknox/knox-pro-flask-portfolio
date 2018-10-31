7234f268868757b41e669577706b4641c97bd70c
Benjamin Knox <bknox.contact@gmail.com>
2018-10-31 16:02:57 +0000

    Tests the csv download javascript

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 from flask_webpack import Webpack
 from flask_dotenv import DotEnv
+from time import sleep
+import random
 
 from api.v1.hero import hero_route
 from api.v1.github import github_route
@@ -25,6 +27,28 @@ webpack.init_app(app)
 @app.route('/<section>')
 def home(section="top"):
   return render_template('app.html', section=section)
+
+@app.route('/test')
+def test():
+  return render_template('test.html')
+
+@app.route('/test.csv')
+def testCsv():
+  sleep(10)
+  csv='header1,header2,header3,header4,header5,header6\n';
+
+  i = 0
+  while i < 4000000:
+    csv+=str(random.randrange(3000,20000)) + ","
+    csv+=str(random.randrange(3000,20000)) + ","
+    csv+=str(random.randrange(3000,20000)) + ","
+    csv+=str(random.randrange(3000,20000)) + ","
+    csv+=str(random.randrange(3000,20000)) + ","
+    csv+=str(random.randrange(3000,20000)) + "\n"
+    i = i + 1
+  
+  return Response(csv, mimetype='text/csv', 
+                  headers={"Content-disposition": "attachment; filename=test.csv"})
 
 # Register REST API routes
 app.register_blueprint(hero_route, url_prefix=apiRoute)
